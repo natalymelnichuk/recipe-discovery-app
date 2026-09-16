8b3667ab117937edde810bc81d2de670e50f1fab
Natallia <ntshmlnchuk@gmail.com>
2026-09-16 17:42:10 -0400

    useLocalStorage completed

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,1 +1,24 @@
 
+import { useState, useEffect } from "react";
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+    const [data, setData] = useState<T>(() => {
+        const dataValue = localStorage.getItem(key);
+
+        if (!dataValue) {
+            return initialValue;
+        } else {
+            const response = JSON.parse(dataValue);
+            return response;
+        }
+    });
+
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(data))
+    }, [data, key])
+
+    return [data, setData];
+
+}
+
+export default useLocalStorage;
